59773bb62f86ca1c7f9ce9c298a4a8cb6fd1313e
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-11-12 03:51:27 +0000

    conversion type

---
+++ day_1_testing_env/03_typeConversion.js
@@ -0,0 +1,20 @@
+// String to Number conversion
+
+let age = "22"
+
+let ageInNumber = Number(age)
+
+console.log(ageInNumber);
+console.log(typeof(ageInNumber));
+
+// Number to String Conversion
+
+let score = "87";
+
+let scoreInString = String(score)
+console.log(scoreInString);
+console.log(typeof(scoreInString));
+
+
+// like this only we have use Boolean, Object etc for type conversion
+
