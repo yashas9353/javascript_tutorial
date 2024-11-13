66c21ca90148974e18669c7aae4dd7dd524384c8
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-11-13 04:20:14 +0000

    string method and prototype

---
+++ day_1_testing_env/06_string.js
@@ -0,0 +1,13 @@
+
+const ename = new String("Yashas");
+
+console.log(ename)
+
+console.log(ename.toLowerCase());
+
+console.log(`Hello ${ename} and lower case is ${ename.toLowerCase()} and length = ${ename.length}`);
+
+// to see all the method/prototype of a datatype or object 
+// go to browser console and enter that object 
+// ex: const ename = new String("Test") then enter ename, now we can see all the method/prototype. 
+
