f15c7a448384bed043cf7c9d26342473f99c3142
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-11-14 03:57:20 +0000

    Number and Math Tutorial

---
+++ day_1_testing_env/07_mathandNums.js
@@ -0,0 +1,24 @@
+let age = 123;
+
+console.log(age.toFixed(2));
+
+console.log(age);
+
+console.log(Number.MAX_VALUE);
+
+console.log(Number.MIN_VALUE);
+
+// Number object also has many property and method which we can use to see use browser console.
+
+// math : Math libraby is a built-in in javascript 
+
+console.log(Math.abs(-12));
+
+console.log(Math.sqrt(25));
+
+// main method is random which range between 0 to 1
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// NOthing more Number object has its own property and methos
+// and Math libraby has its own methods and properties use accordingly
