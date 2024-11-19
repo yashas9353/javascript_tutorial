8126d8d179fd9295661187913dce433ef5d9a224
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-11-19 04:01:56 +0000

    Arrays simple tutorial

---
+++ Basic_2/Arrays_tutorial.js
@@ -0,0 +1,10 @@
+
+let TestArray = [1,"Yashas",2.4];
+console.log(TestArray);
+
+let EmptyArray = new Array(3);
+console.log(EmptyArray);
+
+
+// We have N number of  prototype/method of array we can use  
+
