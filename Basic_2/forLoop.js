11db7128b72a36f448b55408820bcb394e3a5fe4
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-12-16 01:31:20 +0000

    added forloop

---
+++ Basic_2/forLoop.js
@@ -0,0 +1,25 @@
+
+let myArr = [10,30,20];
+
+// for
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+    
+// }
+
+// myArr.forEach((element) => {
+//     console.log(element);
+// });
+
+myArr.sort()
+
+// console.log(myArr);
+
+for (const i in myArr) {
+    console.log(i);
+}
+
+for (const element of myArr) {
+    console.log(element);
+}
