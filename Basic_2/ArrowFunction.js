19cbd5d39f94daa5333667ff430f82eb4c1b28a5
Yashas J <87557094+yashas9353@users.noreply.github.com>
2024-12-16 01:16:15 +0000

    Added Functions

---
+++ Basic_2/ArrowFunction.js
@@ -0,0 +1,14 @@
+myFun();
+
+
+function myFun(){
+    console.log("From MyFun Function");
+    
+}
+
+
+const myArrowFuncxtion = (name) => {
+    console.log(`This is a arraow function ${name}`);
+}
+
+myArrowFuncxtion("Yashas");
